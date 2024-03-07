bf99d2b37aa224f19035a0a6454d158ec3ab78c1
B6238285 <B6238285@g.sut.ac.th>
2024-03-07 15:43:29 +0700

    post Project

--- src/api/project/project.controller.ts
+++ src/api/project/project.controller.ts
@@ -6,12 +6,31 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
+  ClassSerializerInterceptor,
+  UseInterceptors,
 } from '@nestjs/common';
 import { ProjectService } from './project.service';
 import { CreateProjectDto } from './dto/create-project.dto';
 import { UpdateProjectDto } from './dto/update-project.dto';
+import { ApiTags, ApiBearerAuth } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { UserInfo } from '@Shared/decorator/userinfo.decorator';
+import { IUserInfoDecorator } from '@Shared/interface/userinfo.interface';
 
 @Controller('project')
+@ApiTags('project')
+@ApiBearerAuth()
+@UseGuards(JwtAuthGuard)
+@UseInterceptors(ClassSerializerInterceptor)
 export class ProjectController {
   constructor(private readonly projectService: ProjectService) {}
+
+  @Post()
+  async create(
+    @Body() createProjectDto: CreateProjectDto,
+    @UserInfo() userInfo: IUserInfoDecorator,
+  ) {
+    return await this.projectService.createProject(createProjectDto, userInfo);
+  }
 }
--- src/api/project/project.service.ts
+++ src/api/project/project.service.ts
@@ -1,6 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProjectDto } from './dto/create-project.dto';
 import { UpdateProjectDto } from './dto/update-project.dto';
+import { CrudService } from '@Base/crud.service';
+import { ProjectEntity } from '@Database/entities/project.entity';
+import { DataSource, Repository } from 'typeorm';
+import { IUserInfoDecorator } from '@Shared/interface/userinfo.interface';
 
 @Injectable()
-export class ProjectService {}
+export class ProjectService extends CrudService<ProjectEntity> {
+  protected readonly repository: Repository<ProjectEntity>;
+  constructor(private dataSource: DataSource) {
+    super();
+    this.repository = this.dataSource.getRepository(ProjectEntity);
+  }
+
+  async createProject(body: CreateProjectDto, userInfo: IUserInfoDecorator) {
+    const { projectsName } = body;
+    const newProject = await this.repository.save({
+      projectsName: projectsName,
+      userId: userInfo.id,
+    });
+    return newProject;
+  }
+}
